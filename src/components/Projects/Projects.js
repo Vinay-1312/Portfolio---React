3f8fc4e131c8740ce24734cc4ca6ec842bb1f9fb
vinay-1312 <vinaysld123@gmail.com>
2023-11-20 15:56:16 +0000

    package.json

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I've worked on.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
